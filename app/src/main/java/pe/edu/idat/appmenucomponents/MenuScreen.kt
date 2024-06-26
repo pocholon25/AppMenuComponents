4563c78382127d520f00ea7514ea6332b282fe91
pocholon25 <paullopez8578@gmail.com>
2024-06-26 17:20:51 -0500

    SnackBar component

--- app/src/main/java/pe/edu/idat/appmenucomponents/MenuScreen.kt
+++ app/src/main/java/pe/edu/idat/appmenucomponents/MenuScreen.kt
@@ -1,19 +1,33 @@
 package pe.edu.idat.appmenucomponents
 
 import android.annotation.SuppressLint
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.ArrowBack
 import androidx.compose.material.icons.filled.Call
 import androidx.compose.material.icons.filled.MailOutline
+import androidx.compose.material3.Button
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Icon
 import androidx.compose.material3.IconButton
 import androidx.compose.material3.Scaffold
+import androidx.compose.material3.SnackbarDuration
+import androidx.compose.material3.SnackbarHost
+import androidx.compose.material3.SnackbarHostState
 import androidx.compose.material3.Text
 import androidx.compose.material3.TopAppBar
 import androidx.compose.material3.TopAppBarDefaults
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.rememberCoroutineScope
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import kotlinx.coroutines.launch
+
 
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
@@ -52,8 +66,25 @@ fun EjemploToolBar() {
 @SuppressLint("UnusedMaterial3ScaffoldPaddingParameter")
 @Composable
 fun EjemploScaffold() {
-    Scaffold(topBar = { EjemploToolBar() }) {
-    }
+    var estado = remember { SnackbarHostState() }
+    val coroutineScope = rememberCoroutineScope()
+    Scaffold(topBar = { EjemploToolBar() },
+        snackbarHost = {
+            SnackbarHost(hostState = estado)
+        },
+        content = {
+            Box(modifier = Modifier.fillMaxSize(), contentAlignment = Alignment.Center){
+                Button(onClick = {
+                    coroutineScope.launch {
+                        estado.showSnackbar("Hola Clickeaste el boton",
+                            actionLabel = "OK",
+                            duration = SnackbarDuration.Long)
+                    }
+                }) {
+                        Text(text = "Mostrar el Snack Bar")
+                }
+            }
+        })
 }
 
 
