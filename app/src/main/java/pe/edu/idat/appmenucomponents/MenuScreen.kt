d230edf93b05c8aa9fb2ea2f13e7974482ef2ef2
pocholon25 <paullopez8578@gmail.com>
2024-06-26 19:49:34 -0500

    GoogleFont

--- app/src/main/java/pe/edu/idat/appmenucomponents/MenuScreen.kt
+++ app/src/main/java/pe/edu/idat/appmenucomponents/MenuScreen.kt
@@ -6,11 +6,17 @@ import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.ArrowBack
 import androidx.compose.material.icons.filled.Call
+import androidx.compose.material.icons.filled.Email
+import androidx.compose.material.icons.filled.Home
 import androidx.compose.material.icons.filled.MailOutline
+import androidx.compose.material.icons.filled.Person
+import androidx.compose.material3.BottomAppBar
 import androidx.compose.material3.Button
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Icon
 import androidx.compose.material3.IconButton
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.NavigationBarItem
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.SnackbarDuration
 import androidx.compose.material3.SnackbarHost
@@ -23,19 +29,47 @@ import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.rememberCoroutineScope
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.colorResource
+import androidx.compose.ui.text.font.FontFamily
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.unit.sp
 import kotlinx.coroutines.launch
+
+@Composable
+fun EjemploBottomBar() {
+    val items = listOf("Home", "Contactos", "Perfil", "Buson")
+    val iconos = listOf(
+        Icons.Filled.Home,
+        Icons.Filled.Call,
+        Icons.Filled.Person,
+        Icons.Filled.Email)
+    var itemSeleccionado by remember { mutableStateOf(0) }
+    BottomAppBar(
+        containerColor = colorResource(id = R.color.teal_200),
+        contentColor = Color.White
+    ) {
+        items.forEachIndexed { index, item ->
+            NavigationBarItem(
+                selected = itemSeleccionado == index,
+                onClick = { itemSeleccionado = index },
+                icon = { Icon(imageVector = iconos[index], contentDescription = "", tint = Color.Red) },
+                label = { Text(text = item) })
+        }
+    }
+}
 
 
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun EjemploToolBar() {
     TopAppBar(
-        title = { Text(text = "Mi App IDAT") },
+        title = { Text(text = "MiniMarket Julita", style = MaterialTheme.typography.bodyMedium, fontSize = 20.sp, color = Color.White, fontWeight = FontWeight.Bold) },
         colors = TopAppBarDefaults.centerAlignedTopAppBarColors(
-            containerColor = Color.Blue,
+            containerColor =colorResource(id = R.color.teal_200),
             titleContentColor = Color.White
         ),
         navigationIcon = {
@@ -43,7 +77,8 @@ fun EjemploToolBar() {
                 Icon(
                     imageVector = Icons.Filled.ArrowBack,
                     contentDescription = "",
-                    tint = Color.Red)
+                    tint = Color.Red
+                )
             }
         },
         actions = {
@@ -51,13 +86,15 @@ fun EjemploToolBar() {
                 Icon(
                     imageVector = Icons.Filled.Call,
                     contentDescription = "",
-                    tint = Color.Red)
+                    tint = Color.Red
+                )
             }
             IconButton(onClick = { /*TODO*/ }) {
                 Icon(
                     imageVector = Icons.Filled.MailOutline,
                     contentDescription = "",
-                    tint = Color.Red)
+                    tint = Color.Red
+                )
             }
         }
     )
@@ -73,17 +110,22 @@ fun EjemploScaffold() {
             SnackbarHost(hostState = estado)
         },
         content = {
-            Box(modifier = Modifier.fillMaxSize(), contentAlignment = Alignment.Center){
+            Box(modifier = Modifier.fillMaxSize(), contentAlignment = Alignment.Center) {
                 Button(onClick = {
                     coroutineScope.launch {
-                        estado.showSnackbar("Hola Clickeaste el boton",
+                        estado.showSnackbar(
+                            "Hola Clickeaste el boton",
                             actionLabel = "OK",
-                            duration = SnackbarDuration.Long)
+                            duration = SnackbarDuration.Long
+                        )
                     }
                 }) {
-                        Text(text = "Mostrar el Snack Bar")
+                    Text(text = "Mostrar el Snack Bar")
                 }
             }
+        },
+        bottomBar = {
+            EjemploBottomBar()
         })
 }
 
